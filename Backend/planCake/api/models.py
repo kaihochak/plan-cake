148b6fb603a286acdf388fff8af1a72048a74ad5
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2023-11-28 21:33:22 -0700

    created a table

--- Backend/planCake/api/models.py
+++ Backend/planCake/api/models.py
@@ -1,20 +1,23 @@
 from django.db import models
+from datetime import date, time
 
-# Create your models here.
 class FilmEvent(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    date = models.DateField()
-    time = models.TimeField()
-    location = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    film = models.CharField(max_length=100)
-    director = models.CharField(max_length=100)
-    actors = models.CharField(max_length=100)
-    duration = models.CharField(max_length=100)
-    genre = models.CharField(max_length=100)
-    rating = models.CharField(max_length=100)
-    trailer = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)  # Required
+    description = models.CharField(max_length=100, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    time = models.TimeField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    film = models.CharField(max_length=100, blank=True, null=True)
+    director = models.CharField(max_length=100, blank=True, null=True)
+    actors = models.CharField(max_length=100, blank=True, null=True)
+    duration = models.CharField(max_length=100, blank=True, null=True)
+    genre = models.CharField(max_length=100, blank=True, null=True)
+    rating = models.CharField(max_length=100, blank=True, null=True)
+    trailer = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
