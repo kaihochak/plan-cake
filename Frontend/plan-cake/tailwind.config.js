81e21953cf001bd200818c241043d56b25f99103
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2023-12-11 14:38:28 -0700

    Update tailwind.config.js

--- Frontend/plan-cake/tailwind.config.js
+++ Frontend/plan-cake/tailwind.config.js
@@ -9,6 +9,14 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        'xl': '380px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
       fontFamily: {
         'Urbanist': ['"Urbanist"', ...defaultTheme.fontFamily.sans],
       },
