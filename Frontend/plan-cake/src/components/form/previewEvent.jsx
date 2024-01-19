84b387bde2d05cc57cb17dcaad7cbadee9eb3580
Joanna C <joannachowhy@gmail.com>
2024-01-19 00:05:13 -0500

    update preview event page

--- Frontend/plan-cake/src/components/form/previewEvent.jsx
+++ Frontend/plan-cake/src/components/form/previewEvent.jsx
@@ -1,8 +1,41 @@
-const PreviewEvent = (formData) => {
+import React, { useState, useEffect } from "react";
+
+const PreviewEvent = ({ formData }) => {
+    // debug
+    useEffect(() => {
+        console.log(formData);
+    }, [formData]);
+
+    const formattedDate = formData.eventDate instanceof Date ? formData.eventDate.toDateString() : '';
+
     return ( 
     <div>
-        
-    </div> 
+        {/* Event Name */}
+        <div>
+            <p>{formData.eventName}</p>
+        </div>
+
+        {/* Event Description */}
+
+        {/* Date & Time */}
+        <div>
+            <p>Date & Time</p>
+            <p>{formattedDate}</p>
+        </div> 
+
+        {/* Location */}
+        <div>
+            <p>Location</p>
+            <p>{formData.eventLocation}</p>
+        </div> 
+
+        {/* Event Description */}
+        <div>
+            <p>Guests</p>
+            <p>{formData.eventGuestList}</p>
+        </div> 
+
+    </div>
     )
 };
 
