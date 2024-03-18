54c8f1b21f03169fe9169fe388e46f39c4914072
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-03-17 23:00:43 -0600

    try changing banner to max screen width

--- src/_root/pages/FilmPage.jsx
+++ src/_root/pages/FilmPage.jsx
@@ -43,22 +43,22 @@ const FilmPage = () => {
     }
   }, []);
 
-
+  // banner changes based on screen size
   const FilmInfo = () => {
     return (
-      <div className='inset-0 w-[100%] mb-4 '>
+      <div className='inset-0 w-full mb-4'>
         {/* image & title*/}
         <div className='film-img-container'>
           <img src={bannerSrc} alt={film?.title} className='' />
           <div className='film-img-gradient'></div>
         </div>
-        {bp_768 &&<div className='flex justify-start'>
-          <img src={film?.image} alt={film?.title} className='w-[100px]' />
-        </div>}
-
         {/* Info */}
-        <div className='relative'>
-          <div className="absolute -top-20 left-[12.5%]">
+        <div className='relative flex -top-30'>
+          {bp_768 &&
+            <div className='flex justify-start'>
+              <img src={film?.image} alt={film?.title} className='w-[200px]' />
+            </div>}
+          <div className="flex mx-auto">
             <div className='flex flex-col justify-center text-center gap-y-1'>
               <h1 className="text-m-l my-2 font-bold">{film?.title}</h1>
               <p className='text-m-s'>{film?.releasedYear} | {film?.duration}</p>
@@ -70,16 +70,35 @@ const FilmPage = () => {
       </div>
     )
   }
-
   let bannerSrc = film?.image;
   if (bp_768) bannerSrc = film?.banner;
+
+  // Cast
+  const Cast = () => {
+    return (
+      <div className='flex flex-col flex-shrink-0 w-full'>
+        <h2 className='text-m-m text-center mb-2 font-bold'>Cast</h2>
+        <div className="overflow-x-auto">
+          <div className='flex gap-x-3 py-2'>
+            {cast?.map((actor, index) => (
+              <div key={index} className='w-[80px] h-auto flex-shrink-0 relative'>
+                <img src={actor?.image} alt={actor?.name} className='rounded-md inset-0 object-cover mb-2' />
+                <p className='text-m-s'>{actor.name}</p>
+                <p className='text-m-s'>{actor.character}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className='film-container justify-between p-4 mb-20'>
       <div className='film-page-inner'>
         {/* Film Info */}
         {!film ?
-          <div className='flex flex-col gap-2 '>
+          <div className='flex flex-col gap-2'>
             <Skeleton className="w-[250px] h-[400px] rounded-xl" />
             <Skeleton className="w-[250px] h-[20px] rounded-xl" />
             <Skeleton className="w-[250px] h-[20px] rounded-xl" />
@@ -90,40 +109,19 @@ const FilmPage = () => {
           <FilmInfo />}
 
         {/* Cast */}
-        <div className="flex flex-col flex-shrink-0 w-full">
-          <h2 className='text-m-m text-center mb-2 font-bold'>Cast</h2>
-          {!cast ?
-            <div className='flex gap-x-2 '>
-              <Skeleton className="w-[50px] h-[50px] rounded-full" />
-              <Skeleton className="w-[50px] h-[50px] rounded-full" />
-              <Skeleton className="w-[50px] h-[50px] rounded-full" />
-              <Skeleton className="w-[50px] h-[50px] rounded-full" />
-              <Skeleton className="w-[50px] h-[50px] rounded-full" />
-              <Skeleton className="w-[50px] h-[50px] rounded-full" />
-            </div> :
-            <div className="overflow-x-auto">
-              <div className='flex gap-x-3 py-2'>
-                {cast.map((actor, index) => (
-                  <div key={index} className='w-[80px] h-auto flex-shrink-0 relative'>
-                    <img src={actor?.image} alt={actor?.name} className='rounded-md inset-0 object-cover mb-2' />
-                    <p className='text-m-s'>{actor.name}</p>
-                    <p className='text-m-s'>{actor.character}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          }
-        </div>
+        <Cast/>
+        <Cast/>
+        <Cast/>
+        <Cast/>
+        <Cast/>
 
         {/* In Current Events */}
-        <div>
-
-        </div>
+        {/* <div>
+        </div> */}
 
         {/* Similar Films */}
-        <div className="mt-2">
-          {/* FilmList component */}
-        </div>
+        {/* <div className="mt-2">
+        </div> */}
       </div>
     </div>
 
