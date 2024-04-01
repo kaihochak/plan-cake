68c956f4b0e58323804074a6acada24cb4f89107
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-03-31 22:14:29 -0600

    renamed

---
+++ src/components/shared/TransTopbar.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import Logo from '/assets/icons/logo.png';
+import ProfilePlaceholder from '/assets/icons/profile-placeholder.svg';
+import { useUserContext } from "@/context/AuthContext";
+import { useSignOutAccount } from '@/lib/react-query/queries'
+import { IoMdLogOut } from 'react-icons/io';
+
+const TransTopBar = ({isSticky}) => {
+    const { mutate: signOut, isSuccess } = useSignOutAccount();
+    const navigate = useNavigate();
+    const { user } = useUserContext();
+
+    useEffect(() => {
+        if (isSuccess) navigate(0);
+    }, [isSuccess]);
+
+    return (
+        <section className="transTopbar">
+            <nav className="flex-between px-5 py-6">
+                {/* Logo as Home Button */}
+                <Link to="/" className="home-logo">
+                    <img src={Logo} alt="Home" className=" w-32 " />
+                </Link>
+
+                {/* Right side of TransTopBar */}
+                <div className='flex gap-4'>
+                    {/* Logout */}
+                    <button 
+                        variant="ghost"
+                        onClick={() => signOut()}
+                    >
+                        <IoMdLogOut className='text-[30px] text-primary-foreground'/>
+                    </button>
+                    {/* Profile */}
+                    <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
+                        <img 
+                            src={user.imageUrl || ProfilePlaceholder} 
+                            alt="profile"
+                            className="h-8 w-8 rounded-full"
+                        />
+                    </Link>
+                </div>
+            </nav>
+        </section>
+    )
+}
+
+export default TransTopBar
