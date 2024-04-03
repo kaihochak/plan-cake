014b44f1407cfe0becba1237c6ffe3b64c636bc9
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-04-02 21:53:01 -0600

    fix alert dialog

--- src/_root/pages/CreateEvent.jsx
+++ src/_root/pages/CreateEvent.jsx
@@ -9,18 +9,7 @@ import { BsArrowLeft } from "react-icons/bs";
 import { useCreateEvent } from "../../lib/react-query/queries";
 import { useToast } from "@/components/ui/use-toast";
 import { useUserContext } from "@/context/AuthContext";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 
 const CreateEvent = () => {
   const navigate = useNavigate();
@@ -33,57 +22,56 @@ const CreateEvent = () => {
     date: "",
     location: "",
     guestList: [],
-    selectedFilms: [], 
+    selectedFilms: [],
     file: [],
     imageUrl: "",
     imageId: "",
   });
   const [newPost, setNewPost] = useState(null);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   // Query to create a new event
   const { mutateAsync: createEventToDB, isPending: isLoadingCreate } = useCreateEvent();
 
-  //  Go to the previous step
-  const prevStep = () => {
-    // If we're not on the first step, go to the previous step
-    if (currentStep > 0) { setCurrentStep(currentStep - 1); }
-    // If we're on the first step, prompt the user to confirm leaving the page
-    else {
-      setIsDialogOpen(true);     
-      }
-    };
-
+  // Alert dialog for canceling event creation
   const Alert = () => {
     return (
-        <AlertDialog>
-          <AlertDialogTrigger>Open</AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your account
-                and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      )
+      <AlertDialog>
+        <AlertDialogTrigger>
+          <button className="text-m-2xl ml-1 mt-10 mb-6" onClick={prevStep}>
+            <BsArrowLeft />
+          </button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={() => navigate("/")}>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    )
   }
+
+  //  Go to the previous step
+  const prevStep = () => {
+    if (currentStep > 0) { setCurrentStep(currentStep - 1); } // If we're not on the first step, go to the previous step
+  };
 
   // Go to the next step
   const nextStep = (formData) => {
-    if (formData) { setFormData({ ...formData, ...formData }); } 
-    
+    if (formData) { setFormData({ ...formData, ...formData }); }
+
     // submit form data to create event
-    if (currentStep === 3) { 
+    if (currentStep === 3) {
       // wait until loading is done
       if (!isLoadingCreate) {
-        handleSubmit(formData); 
+        handleSubmit(formData);
       } else {
         return;
       }
@@ -125,23 +113,23 @@ const CreateEvent = () => {
       <div className="flex flex-col w-full max-w-[1024px] mx-auto">
         <header className="w-full">
           {/* Back Button */}
-          <button className="text-m-2xl ml-1 mt-10 mb-6" onClick={prevStep}>
-            {/* <BsArrowLeft /> */}
-            <BsArrowLeft />
-          </button>
+          {currentStep !== 0 ? (
+            <button className="text-m-2xl ml-1 mt-10 mb-6" onClick={prevStep}>
+              <BsArrowLeft />
+            </button>)
+            : <Alert />
+          }
+
         </header>
 
         {/* Current Step Content */}
         <div className="w-full">
-          {currentStep === 0 && <CreateEventType formData={formData} nextStep={nextStep}/>}
-          {currentStep === 1 && <EventDetails formData={formData} nextStep={nextStep}/>}
-          {currentStep === 2 && <FilmSearch formData={formData} nextStep={nextStep}/>}
-          {currentStep === 3 && <PreviewEvent formData={formData} nextStep={nextStep} isLoadingCreate={isLoadingCreate}/>}
-          {currentStep === 4 && <ConfirmedEvent formData={formData} goToEventPage={nextStep}/>}
+          {currentStep === 0 && <CreateEventType formData={formData} nextStep={nextStep} />}
+          {currentStep === 1 && <EventDetails formData={formData} nextStep={nextStep} />}
+          {currentStep === 2 && <FilmSearch formData={formData} nextStep={nextStep} />}
+          {currentStep === 3 && <PreviewEvent formData={formData} nextStep={nextStep} isLoadingCreate={isLoadingCreate} />}
+          {currentStep === 4 && <ConfirmedEvent formData={formData} goToEventPage={nextStep} />}
         </div>
-
-        {isDialogOpen && <Alert />}
-
       </div>
     </section>
   );
