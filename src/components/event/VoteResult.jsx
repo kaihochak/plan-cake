b1cf13ae8781c31c16e69af9e290109caede7614
Joanna C. <joannachowhy@gmail.com>
2024-05-05 00:27:15 -0400

    update

--- src/components/event/VoteResult.jsx
+++ src/components/event/VoteResult.jsx
@@ -2,9 +2,49 @@ import React, { useEffect, useState } from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { toast } from "@/components/ui/use-toast"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+const FormSchema = z.object({
+  film: z.string().min(1, {
+    message: "You have to select at least one item.",
+  }),
+})
+
 
 const VoteResult = ({ formData }) => {
   const [results, setResults] = useState([])
+  const [checkedFilm, setCheckedFilm] = useState("")
+
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      film:""
+    },
+  })
+
+  function onSubmit(data) {
+    console.log("submit");
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
 
   /************************************************************************
    *  getVotes
@@ -34,6 +74,12 @@ const VoteResult = ({ formData }) => {
     } 
     return [];
   }
+
+  const handleConfirmFilm = () => {
+    console.log();
+
+  }
+
 
   /************************************************************************
    *  populate results
@@ -72,6 +118,20 @@ const VoteResult = ({ formData }) => {
 
   return (
     <div className='p-6'>
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <FormField
+        control={form.control}
+        name="items"
+        render={() => (
+          <FormItem>
+            <div className="mb-4">
+              <FormLabel className="text-base">Sidebar</FormLabel>
+              <FormDescription>
+                Select the items you want to display in the sidebar.
+              </FormDescription>
+            </div>
+          
       <Table>
         <TableHeader>
           <TableRow>
@@ -84,8 +144,36 @@ const VoteResult = ({ formData }) => {
         <TableBody>
           {/* map over the formData.selectedFilms */}
           {results.map((film, index) => (
+
+
             <TableRow key={index}>
-              <TableCell className=""> <Checkbox /></TableCell>
+              <TableCell className=""> 
+              <FormField
+              key={film.id}
+              control={form.control}
+              name="film"
+              render={({ field }) => {
+                return (
+                  <FormItem
+                    key={film.id}
+                    className="flex flex-row items-start space-x-3 space-y-0"
+                  >
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                        {...field}
+                      />
+
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      {film.label}
+                    </FormLabel>
+                  </FormItem>
+                )
+              }}
+            />
+              </TableCell>
               <TableCell>{film.title}</TableCell>
               <TableCell>{film.votes}</TableCell>
               <TableCell className="text-right">{film.voters.map((voter, index) => (
@@ -95,8 +183,17 @@ const VoteResult = ({ formData }) => {
           ))}
         </TableBody>
       </Table>
-
-      <Button className="">Confirm film</Button>
+      <FormMessage />
+            </FormItem>
+          )}
+        />
+      <Button 
+        type="submit"
+        className=""
+        // onClick={handleConfirmFilm}
+      >Confirm film</Button>
+      </form>
+    </Form>
     </div>
   )
 }
