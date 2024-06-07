349137f4f2e43e61961f7e793d82ad5e459ef17d
Joanna C. <joannachowhy@gmail.com>
2024-06-06 23:19:09 -0400

    updated confirmed film ui

--- src/components/event/ConfirmedFilm.jsx
+++ src/components/event/ConfirmedFilm.jsx
@@ -19,36 +19,31 @@ const ConfirmedFilm = ({confirmedFilm, guestList}) => {
     
     return (
         <div>
-          <div className='inset-0 w-full mb-4 md:mb-0'>
             {/* image & title*/}
-            <div className='film-img-container'>
+            <div className='confirmedfilm-img-container z-10'>
               {bannerSrc && <img src={bannerSrc} alt={confirmedFilm?.title} className='film-img' />}
               {/* fade mask */}
               <div className='film-img-mask'></div> 
             </div>
-            <div className='flex justify-start w-[140px]'>
-              <div>
+            <div className='z-50'>
+              <img src={confirmedFilm?.poster_path ? image500(confirmedFilm?.poster_path) : fallbackMoviePoster} alt={confirmedFilm?.title} className='' />
+
+            </div>
+          {/* <div className='inset-0 w-full mb-4 md:mb-0'>
+            <div className='flex flex-rol'>
+              <div className=''>
                 <img src={confirmedFilm?.poster_path ? image500(confirmedFilm?.poster_path) : fallbackMoviePoster} alt={confirmedFilm?.title} className='' />
               </div>
               <div>
-                
+                <span>Selected Film</span>
+                <p>{confirmedFilm?.title === confirmedFilm?.original_title ? confirmedFilm.title : confirmedFilm.original_title + " (" + confirmedFilm.title + ")"}</p>
               </div>
 
             </div>
     
-            {/* Info */}
-            <div className='relative flex -mb-24 gap-x-8 -top-20 sm:-top-36 sm:-mb-36 md:-top-72 md:-mb-60'>
-              {/* image */}
-           
-              <div className="flex mx-auto">
-                <div className='flex flex-col justify-center gap-y-1 md:gap-y-2 '>
-                  {/* title */}
-                  <h1 className="text-m-l md:text-[30px] md:my-4 my-2 font-bold ">{confirmedFilm?.title === confirmedFilm?.original_title ? confirmedFilm.title : confirmedFilm.original_title + " (" + confirmedFilm.title + ")"}</h1>
-    
-                </div>
-              </div>
-            </div>
-          </div>
+          </div> */}
+
+
 
             <div className='p-4'>
                 {/* Confirmed Film */}
