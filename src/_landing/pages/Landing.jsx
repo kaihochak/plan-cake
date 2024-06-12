4227cac1049cf7180c8ffd492e8efb69bba59296
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-06-11 20:41:18 -0600

    little css tweak

--- src/_landing/pages/Landing.jsx
+++ src/_landing/pages/Landing.jsx
@@ -5,7 +5,7 @@ import FAQ from '@/components/landing/FAQ'
 
 const Landing = () => {
   return (
-    <div className='flex flex-col w-full mx-auto'>
+    <div className='flex flex-col w-full mx-auto overflow-x-hidden'>
       <Features />
       <About /> 
       <FAQ />
