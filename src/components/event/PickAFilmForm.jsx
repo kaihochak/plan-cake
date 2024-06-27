1112ada81da845aa39b39940ea84456594145250
Joanna C. <joannachowhy@gmail.com>
2024-06-26 23:41:44 -0400

    add time picker

--- src/components/event/PickAFilmForm.jsx
+++ src/components/event/PickAFilmForm.jsx
@@ -7,14 +7,23 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { cn } from "@/lib/utils";
 import { CalendarIcon } from "@radix-ui/react-icons";
-import { addDays, format } from "date-fns";
+import { addDays, format, min } from "date-fns";
 import { Calendar } from "@/components/ui/calendar";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useToast } from "@/components/ui/use-toast";
 import { useNavigate } from 'react-router-dom'
 import { useCreatePickAFilm } from '@/lib/react-query/queries'
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+  } from "@/components/ui/timeSelect"
 
 const formSchema = z.object({
 	title: z.string().optional(),
@@ -28,6 +37,8 @@ const PickAFilmForm = ({ isOpen, onClose }) => {
 	const [formData, setFormData] = useState({
 		title: "",
 		date: "",
+		hour: "",
+		minute: "",
 		host: ""
 	})
 	const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
@@ -105,6 +116,12 @@ const PickAFilmForm = ({ isOpen, onClose }) => {
 		}
 	};
 
+	useEffect(() => {
+		console.log("formData", formData.date);
+		console.log("formData", formData.date.toString().slice(0, 2));
+		// console.log("after insert hour", formData.date && formData.date.split(" ")[3]);
+	}, [formData]);
+
 	/*****************************************************************************
 	 * Rendering
 	 *****************************************************************************/
@@ -148,6 +165,8 @@ const PickAFilmForm = ({ isOpen, onClose }) => {
 							)}
 						/>
 						{/* Date */}
+						<div className='flex gap-x-2 items-end'>
+
 						<Popover
 							open={isDatePickerOpen}
 							onOpenChange={setIsDatePickerOpen}
@@ -156,7 +175,7 @@ const PickAFilmForm = ({ isOpen, onClose }) => {
 								<Button
 									variant='input'
 									className={cn(
-										"w-full rounded-none justify-start text-left font-normal bg-primary border-b-2 h-14 p-2 text-primary-foreground",
+										"w-full rounded-none justify-start text-left font-normal bg-primary border-foreground-dark border-b-2 h-14 p-2 text-primary-foreground",
 										!formData.date ? "text-input" : ""
 									)}
 								>
@@ -178,12 +197,72 @@ const PickAFilmForm = ({ isOpen, onClose }) => {
 										onSelect={(date) => {
 											setIsDatePickerOpen(false);
 											setFormData({ ...formData, date: date })
+					
 										}}
 									/>
 								</div>
 							</PopoverContent>
 						</Popover>
-						<Button type="submit" variant="select" className="">Create Event</Button>
+						<Select
+							value={formData.hour}
+							onValueChange={(value) => setFormData({ ...formData, hour: value })}
+						>
+							<SelectTrigger className="w-[20%]">
+								<SelectValue placeholder="HH" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+								<SelectItem value="12">00</SelectItem>
+								<SelectItem value="01">01</SelectItem>
+								<SelectItem value="02">02</SelectItem>
+								<SelectItem value="03">03</SelectItem>
+								<SelectItem value="04">04</SelectItem>
+								<SelectItem value="05">05</SelectItem>
+								<SelectItem value="06">06</SelectItem>
+								<SelectItem value="07">07</SelectItem>
+								<SelectItem value="08">08</SelectItem>
+								<SelectItem value="09">09</SelectItem>
+								<SelectItem value="10">10</SelectItem>
+								<SelectItem value="11">11</SelectItem>
+								<SelectItem value="12">12</SelectItem>
+								<SelectItem value="13">13</SelectItem>
+								<SelectItem value="14">14</SelectItem>
+								<SelectItem value="15">15</SelectItem>	
+								<SelectItem value="16">16</SelectItem>
+								<SelectItem value="17">17</SelectItem>
+								<SelectItem value="18">18</SelectItem>
+								<SelectItem value="19">19</SelectItem>
+								<SelectItem value="20">20</SelectItem>
+								<SelectItem value="21">21</SelectItem>
+								<SelectItem value="22">22</SelectItem>
+								<SelectItem value="23">23</SelectItem>
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+						<Select>
+							<SelectTrigger className="w-[20%]">
+								<SelectValue placeholder="MM" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+								<SelectItem value="00">00</SelectItem>
+								<SelectItem value="05">05</SelectItem>
+								<SelectItem value="10">10</SelectItem>
+								<SelectItem value="15">15</SelectItem>
+								<SelectItem value="20">20</SelectItem>
+								<SelectItem value="25">25</SelectItem>
+								<SelectItem value="30">30</SelectItem>
+								<SelectItem value="35">35</SelectItem>
+								<SelectItem value="40">40</SelectItem>
+								<SelectItem value="45">45</SelectItem>
+								<SelectItem value="50">50</SelectItem>
+								<SelectItem value="55">55</SelectItem>
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+
+						</div>
+						<Button type="submit" variant="select" className="w-full">Create Event</Button>
 					</form>
 				</Form>
 			</DialogContent>
